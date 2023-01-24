f469ca0c11fc9e6e6d05c6b11aafcf03c047fe87
Hashin Mohammed <hashinpks@gmail.com>
2023-01-25 01:22:46 +0530

    first commit netflix-clone

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import Row from './Row';
-import requests from './requests';
+import React from "react";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>hey hashin</h1>
-      <Row title='NETFLIX ORIGINALS' fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+    <div style={{ textAlign: "center" }}>
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
- 
 }
 
 export default App;
--- src/Row.jsx
+++ src/Row.jsx
@@ -4,7 +4,7 @@ import  axios  from './axios';
 import "./Row.css"
 const base_url = "https://image.tmdb.org/t/p/original/";
 function Row(props) {
-  const { title, fetchUrl } = props;
+  const { title, fetchUrl,isLargeRow } = props;
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     async function fetchData() {
@@ -25,7 +25,9 @@ function Row(props) {
       <div className="row__posters">
         {movies.map(movie=>{
           return(
-            <img className='row__poster'  src={`${base_url}${movie.poster_path}`}alt={movie.name} />
+            <img 
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}src={`${base_url}${isLargeRow ? movie.poster_path :movie.backdrop_path}`}alt={movie.name} />
           )
         })}
         </div>
